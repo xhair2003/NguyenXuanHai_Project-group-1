73e0606275c386e0aec72f27d1a55d4de98be483
xhair2003 <nngghai2003@gmail.com>
2022-05-30 16:10:28 +0700

    update 30/05/2022

--- TesterOwner.cpp
+++ TesterOwner.cpp
@@ -1,12 +1,49 @@
 #include<iostream>
 #include<algorithm>
+#include<vector>
+#include<string.h>
+#include<bits/stdc++.h>
 using namespace std;
-
+static int checkOwner = 0;
 class FootballClub {
 	private:
 		string idMembers;
 		string contractTerm;
 	public:
+		bool checkDate(int day, int month, int year) {
+		switch (month) {
+			case 1 :
+			case 3:
+			case 5 :
+			case 7 :
+			case 8 :
+			case 10 :
+			case 12 :
+					if(day<1 || day > 31) {
+						return false;
+					}
+					break;
+			case 4 :
+			case 6 :
+			case 9 :
+			case 11 :
+				if(day < 1 || day > 30) {
+					return false;
+				}
+				break;
+			case 2 :
+				if(day < 1 || day > 29) {
+					return false;
+				}
+				break;
+			default :
+				return false;
+		}
+		if(year <= 2022) {
+			return false;
+		} 
+		return true;
+		}
 		void setMember(string idMembers){
 			this->idMembers = idMembers;
 		}
@@ -24,17 +61,30 @@ class FootballClub {
 		this->contractTerm = " ";
 	}
 	void input(){
-		cout<<"----------------------------------------------------------"<<endl;
-		cout<<"==============ENTER THE PROLIFE FOOTBALL CLUB============="<<endl;
 		cout<<"Enter the ID.Members: ";
 		cin>>this->idMembers;
+		for(int i=0;i<=10;i++){
+			if(this->idMembers[i]>=97&&this->idMembers[i]<=122)
+				this->idMembers[i]-=32;
+		}
 		cin.ignore();
-		cout<<"Enter the ContractTerm: ";
-		getline(cin,this->contractTerm);
+		cout<<"Enter the ContractTerm(Format DD/MM/YYYY): \n\n";
+		int day,month,year;
+		do {
+			cout << "Day : ";
+			cin >> day;
+			cout << "Month : ";
+			cin >> month;
+			cout << "Year : ";
+			cin >> year;
+		}while(checkDate(day,month,year)==false);
+		string sday,smonth,syear;
+		sday = to_string(day);
+		smonth = to_string(month);
+		syear = to_string(year);
+		this->contractTerm = sday+'/'+smonth+'/'+syear;
 	}
 	void output(){
-		cout<<"----------------------------------------------------------"<<endl;
-		cout<<"=============OUTPUT THE PROLIFE FOOTBALL CLUB============="<<endl;
 		cout<<"*ID of Members: "<<this->idMembers<<endl;
 		cout<<"*Contract Term: "<<this->contractTerm<<endl;
 	}
@@ -45,7 +95,6 @@ class FootballClub {
 	virtual void oppRenewContract() = 0;
 	virtual bool valueBringingLastSeason() = 0;
 };
-
 class Owner:public FootballClub{
 	private:
 		string corporTaxCode;//ma thue doanh nghiep
@@ -101,12 +150,15 @@ class Owner:public FootballClub{
 		this->investmentAmount=0;
 	}
 	void input(){
+		cout << endl << endl << "INPUT PROFILE : "<< endl << endl;
 		FootballClub::input();
-		cout<<"----------------------------------------------------------"<<endl;
-		cout<<"============INPUT THE PROFILE OF OWNER============"<<endl;
 		cout<<"Enter the Corpor TaxCode: ";
 		cin>>this->corporTaxCode;
 		cin.ignore();
+		for(int i=0;i<=10;i++){
+			if(this->corporTaxCode[i]>=97&&this->corporTaxCode[i]<=122)
+				this->corporTaxCode[i]-=32;
+		}
 		cout<<"Enter the Net Worth: ";
 		cin>>this->netWorth;
 		cout<<"Enter the Net Profit: ";
@@ -119,10 +171,12 @@ class Owner:public FootballClub{
 		cout<<"Enter the Investment Amount: ";
 		cin>>this->investmentAmount;
 	}
+	
+	//
+	
 	void output(){
+		cout << endl << endl << "PROFILE : " << endl << endl;
 		FootballClub::output();
-		cout<<"----------------------------------------------------------"<<endl;
-		cout<<"===========OUTPUT THE PROFILE OF OWNER============"<<endl;
 		cout<<"*Corpor TaxCode: "<<this->corporTaxCode<<endl;
 		cout<<"*Net Worth: "<<this->netWorth<<endl;
 		cout<<"*Net Profit: "<<this->netProfit<<endl;
@@ -130,32 +184,48 @@ class Owner:public FootballClub{
 		cout<<"*Public Debt: "<<this->publicDebt<<endl;
 		cout<<"*Investment Amount: "<<this->investmentAmount<<endl;
 	}
+	
+	// ROE da hoan thien
+	
 	double ROE(){
 		double roe=this->netProfit/this->netWorth;
 		return roe;
 	}
+	
+	// bankruptcyRisk da hoan thien
+	
 	bool bankruptcyRisk(){
-		if(this->publicDebt>=(this->netProfit+this->netWorth)||this->ROE()<0.02)
+		if(this->publicDebt>=(this->netProfit+this->netWorth) && this->ROE()<0.02)
 			return true;
 		else 
 			return false;
 	}
+	
+	// mission da hoan thien
+	
 	string mission(){
 		string mis = "Make club success!!";
 		return mis;
 	}
+	
+	// calculateWage chua hoan thien
+	
 	long long calculateWage(){
-		//chua hoan thien
 		double cal=this->netProfit-(this->investmentAmount+this->publicDebt);
 		return cal;
 	}
+	
+	//signingCondition da hoan thien
+	
 	bool signingCondition(){
-		//publicDebt & ROE 
-		if (this->publicDebt<((this->netProfit+this->netWorth)/10) && this->ROE()>0.15)
+		if (this->publicDebt<=((this->netProfit+this->netWorth)*2) && this->ROE()>0.15 && this->getAmount()!=0)
 			return true;
 		else 
 			return false;
 	}
+	
+	//riskOfTerminateContract chua hoan thien
+	
 	void riskOfTerminateContract(){
 		cout<<"*Risk Of Terminate Contract: ";
 		//ROE & publicDebt & calWage
@@ -163,7 +233,10 @@ class Owner:public FootballClub{
 			cout<<"\n-Contract Terminated!!\n";
 		}else 
 			cout<<"\n-The contract is still valid!!\n";
-	}	
+	}
+	
+	// oppRenewContract chua hoan thien
+		
 	void oppRenewContract(){
 		cout<<"*OOP Renew Contract: ";
 		if(this->signingCondition()==true){
@@ -175,36 +248,80 @@ class Owner:public FootballClub{
 		}else 
 			cout<<"\nThe contract has been terminated!!!\n";
 	}
+	
+	// valueBringingLastSeason chua hoan thien
+	
 	bool valueBringingLastSeason(){
-		return true;
+		return false;
 	}
 };
 class ListOwner{
+//	private:
+
 	public: 
-		Owner on[100];
 		int n;
-	void Listin(){
-		cout<<"Enter the number of candidate owners: ";
+		vector<Owner> v;
+		
+	void inputList(){
 		cin>>n;
-		for(int i=1;i<=n;i++){
-			cout<<"Owner "<<i<<" : "<<endl;
-			on[i].input();
+		cout << "\n\nEnter the information of each candidate : \n\n";
+		for(int i=0;i<n;i++){
+			cout<<"\n\nCandidate "<<i+1<<" : ";
+			Owner on;
+			on.input();
+			v.push_back(on);
 		}
 	}
-	void Listout(){
-		cout<<"----------------------------------------------------------------"<<endl;
-		for(int i=0;i<=n;i++){
-			on[i].output();
-			on[i].riskOfTerminateContract();
+	void outputList(){
+		cout<<"----------------------------------------------------------------"<<endl<<endl<<endl;
+		cout << "Detailed list of each candidate :\n\n";
+		for(int i=0;i<v.size();i++){
+			v[i].output();
 		}
 	}
+	
+	void chooseTheOwner() {
+		for(int i=0 ; i<v.size() ; i++) {
+			if(v[i].bankruptcyRisk()==true || v[i].signingCondition()==false) {
+				v.erase(v.begin()+i);
+				i--;
+			}
+		}
+		
+		if(v.size()!=0) {
+			checkOwner++;
+			for(int i=0 ; i<v.size() ; i++) {
+			for(int j=i ; j<v.size() ; j++) {
+				if(v[i].getAmount() < v[j].getAmount()) {
+					Owner tmp = v[i];
+					v[i] = v[j];
+					v[j] = tmp;
+				}
+			}
+			}
+			while(v.size()!=1) {
+			v.pop_back();
+			}
+		} else {
+				cout << "\n\noop!!!There are no suitable candidates in all that you have entered !!!\n\nWe need to find other candidates\n\nEnter the number of candidates you have selected : ";
+			}
+		}
+		
+		void scriptOfOwner() {
+			cout << "Our team currently has no owner !!! \n\nYou need to enter the number of potential candidates for the position of team owner : ";
+			this->inputList();
+			this->outputList();
+			this->chooseTheOwner();
+			while(checkOwner==0) {
+				this->inputList();
+				this->chooseTheOwner();
+			}
+			cout << "\n\nWe reviewed each person's details and the fan vote took place !!!\n\nWe would like to announce the official owner of the team is Mr."<<v[0].getnameOwner();
+		}
 };
 int main() {
-	Owner on[100];
 	ListOwner Lo;
-	Lo.Listin();
-	Lo.Listout();
-	
+	Lo.scriptOfOwner();
 	return 0;
 }
 
